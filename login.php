1d2dc112d72f72720a9611c265306eea1ed9eb66
Abasol <ramzeeesss666@gmail.com>
2023-05-27 17:13:50 +0800

    login interface enhanced

--- login.php
+++ login.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/style.css">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="assets/style.css?<?php echo time(); ?>" />
+    <title>Facebook - log in or sign up</title>
 </head>
 <body>
 
+<div class="login-page">
 <main>
     <section>
     <div class="main-login">
@@ -19,24 +20,28 @@
                </div>
                 <p>Connect with friends and the world <br>around you on Facebook</p>
             </div>
-            <div class="login-form">
-                <input type="text" name="" id="">
-                <input type="text" name="" id="">
-                <button class="loggin">Log In</button>
-                <a>Forgot password?</a>
-                <button class="create">Create new account</button>
+            <div>
+        <form action="" method="post" class="login-form">
+            <input type="text" name="" id="" placeholder="Email or phone Number">
+            <input type="password" name="" id="" placeholder="Password">
+            <button class="loggin">Log In</button>
+             <a href="http://" target="_blank" rel="noopener noreferrer" class="fpass">Forgot Password?</a>
+            <button class="create">Create new account</button>
+        </form>
+
+            <div class="create-page">
+              <a href="http://">Create a Page</a> for a celebrity, brand or business.
+            </div>
             </div>
     </div>
     </section>
-  
-</main>
 
+</main>
 <footer>
     <div class="login-footer">
     <h1>hello i am a main footer of login page</h1>
     </div>
 </footer>
-
-    
+</div>
 </body>
 </html>
