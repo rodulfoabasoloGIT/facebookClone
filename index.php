29a4ff59507fa47bf3caf2b4377721d4088056fe
Abasol <ramzeeesss666@gmail.com>
2023-05-27 16:18:34 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="assets/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Facebook</title>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <div class="parent">
+          <div class="fb-logo">
+        <a href="http://" target="_blank" rel="noopener noreferrer">
+        <img src="assets/images/wtf.png" />
+        </a>
+          </div>
+          <div class="fb-search">
+          <i class="fa fa-search"></i>  
+          <input type="search" placeholder="Search Facebook"/>
+          </div>
+          <div class="fb-menu">
+            <div class="menu-icon">
+              <i class="fab fa-facebook-messenger"></i>
+            </div>
+          <div class="menu-icon">
+          <i class="fa-solid fa-bell"></i>
+          </div>
+          <div class="menu-icon">
+          <i class="fa-solid fa-user"></i>
+          </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<main>
+    <section>
+    <div class="main-login">
+    <div class="login-parent">
+            <div class="fb-login">
+               <div class="login-logo-contain">
+                    <img src="assets/images/wtf.png"/>
+               </div>
+                <p>Connect with friends and the world <br>around you on Facebook</p>
+            </div>
+            <div class="login-form">
+                <input type="text" name="" id="">
+                <input type="text" name="" id="">
+                <button class="loggin">Log In</button>
+                <a>Forgot password?</a>
+                <button class="create">Create new account</button>
+            </div>
+    </div>
+    </section>
+  
+</main>
+
+<footer>
+    <div class="login-footer">
+    <h1>hello i am a main footer of login page</h1>
+    </div>
+</footer>
+
+    
+</body>
+</html>
